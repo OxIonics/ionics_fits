6cfc8166eeb77aebd89fe9915cdb62be9dd9d1fe
hartytp <thomas.peter.harty@gmail.com>
2022-09-13 23:06:58 +0100

    pytype

--- ionics_fits/models/utils.py
+++ ionics_fits/models/utils.py
@@ -2,7 +2,7 @@ import copy
 import dataclasses
 import numpy as np
 from scipy import signal
-from typing import Dict, Tuple, TYPE_CHECKING
+from typing import Dict, Optional, Tuple, TYPE_CHECKING
 from ..common import Model, ModelParameter
 from ..utils import Array
 
@@ -38,7 +38,7 @@ class MappedModel(Model):
         self,
         inner: Model,
         mapped_params: Dict[str, str],
-        fixed_params: Dict[str, float] = None,
+        fixed_params: Optional[Dict[str, float]] = None,
     ):
         """Init
 
