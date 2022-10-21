c47a999449aef201aab19507b4e198bfef12c614
Clemens Loschnauer <clemens.loschnauer@oxionics.com>
2022-10-21 15:37:18 +0100

    rabi: update docstring and parameter names

--- ionics_fits/models/rabi.py
+++ ionics_fits/models/rabi.py
@@ -16,56 +16,45 @@ class RabiFlop(Model):
     """
     Base class for damped Rabi flops
 
-    The model function of this class calculates the probability P(E) of
-    observing some readout event "E" after a system comprised of two states,
-    labelled |g> and |e>, has interacted with a sinusoidal driving field. This
+    The model function of this class calculates the readout observation
+    probability while driving Rabi flops between two states |g> and |e>. This
     probability is given by
-        P(E) = P_readout_g + [P_readout_e - P_readout_g] * P_e
-    where P_e is the time-dependent population in state |e>, while P_readout_g
-    and P_readout_e denote the readout levels of the states. Explicitly,
-    P_readout_i is the conditional probability P(E|i) that the readout event
-    is observed given that the particle occupies state |i>.
+        P = P_readout_g + (P_readout_e - P_readout_g) * P_e
+    where P_e is the time-dependent population in the excited state, while
+    P_readout_g and P_readout_e denote the individual readout levels.
 
-    This model requires that the particle starts out either in |g> or in |e>,
-    specified by passing :param start_excited: to the __init__ method. The
+    This model requires that the system starts out in either |g> or |e>,
+    specified by passing :param:`start_excited` to :meth:`__init__`. The
     probability of transition from one state to the other may then be
     calculated as
         P_trans = 1 / 2 * omega^2 / W^2 * [1 - exp(-t / tau) * cos(W * t)]
     where
         - t is the duration of interaction between qubit and driving field
         - W = sqrt(omega^2 + delta^2)
-        - delta is the detuning of driving field from resonance frequency of qubit
+        - delta is the detuning of driving field from resonance frequency
         - omega is the Rabi frequency
         - tau is the decay time constant
-
-    The event "E" may for example be defined as "ion bright", but this is not
-    required by the model. The user only needs to provide the state which the
-    particle initially occupies.
 
     This class does not support fitting directly, use one of the subclasses
     :class RabiFlopFreq: or :class RabiFlopTime: instead.
 
     Independent variables:
-        - t_pulse: Nominal pulse duration of driving field
-        - freq: Frequency of driving field
+        - t_pulse: Duration of driving pulse including dead time. The true
+            duration of interaction is calculated as t = max(0, t_pulse - t_dead).
+        - w: Angular frequency of driving pulse. The detuning is calculated as
+            delta = w - w_0.
 
     Model parameters:
-        - P_readout_e: Probability P(E|e) of observation of readout event
-            when particle occupies state |e> (fixed to 1 by default)
-        - P_readout_g: Probability P(E|g) of observation of readout event
-            when particle occupies state |g> (fixed to 0 by default)
+        - P_readout_e: Readout level for state |e> (fixed to 1 by default)
+        - P_readout_g: Readout level for state |g> (fixed to 0 by default)
         - omega: Rabi frequency
         - tau: Decay time constant (fixed to infinity by default)
-        - t_dead: Dead time for driving field (fixed to 0 by default). The
-            true duration of interaction is calculated as t = max(0, t_pulse - t_dead).
-        - freq_offset: Offset of resonance from zero of frequency variable.
-            The true detuning is calculated as delta = freq - freq_offset.
+        - t_dead: Dead time (fixed to 0 by default).
+        - w_0: Offset of resonance from zero of angular frequency variable.
 
     Derived parameters:
-        - t_pi: Pi-time in the absence of dead-time,
-            calculated as t_pi = pi / omega
-        - t_pi_2: Pi/2-time in the absence of dead-time,
-            calculated as t_pi_2 = t_pi / 2
+        - t_pi: Pi-time, calculated as t_pi = pi / omega
+        - t_pi_2: Pi/2-time, calculated as t_pi_2 = t_pi / 2
 
     All frequencies and detunings are in angular units.
     """
@@ -93,30 +82,23 @@ class RabiFlop(Model):
             scale_func=lambda x_scale, y_scale, _: y_scale,
         ),
         omega: ModelParameter(lower_bound=0.0),
-        tau: ModelParameter(
-            lower_bound=0.0,
-            fixed_to=np.inf,
-        ),
-        t_dead: ModelParameter(
-            lower_bound=0.0,
-            fixed_to=0.0,
-        ),
-        freq_offset: ModelParameter(fixed_to=0.0),
+        tau: ModelParameter(lower_bound=0.0, fixed_to=np.inf),
+        t_dead: ModelParameter(lower_bound=0.0, fixed_to=0.0),
+        w_0: ModelParameter(fixed_to=0.0),
     ) -> Array[("num_samples",), np.float64]:
         """
         Return the probability of observation of readout event.
 
-        :param x: Tuple (t_pulse, freq) of ndarrays containing nominal
-            pulse duration and frequency of driving field. They must have
-            shapes such that they are broadcastable.
+        :param x: Tuple (t_pulse, w) of ndarrays containing pulse duration and
+            angular frequency of driving field. They must have shapes such
+            that they are broadcastable.
         """
         t = np.clip(x[0] - t_dead, a_min=0.0, a_max=None)
-        delta = x[1] - freq_offset
+        delta = x[1] - w_0
         W = np.sqrt(omega**2 + delta**2)
 
         P_trans = (
-            1
-            / 2
+            0.5
             * np.divide(omega**2, W**2, out=np.zeros_like(W), where=(W != 0.0))
             * (1 - np.exp(-t / tau) * np.cos(W * t))
         )
@@ -180,11 +162,16 @@ class RabiFlopFreq(RabiFlop):
         self.parameters["omega"].scale_func = lambda x_scale, y_scale, _: x_scale
         self.parameters["tau"].scale_func = lambda x_scale, y_scale, _: x_scale
         self.parameters["t_dead"].scale_func = lambda x_scale, y_scale, _: 1 / x_scale
-        self.parameters["freq_offset"].scale_func = lambda x_scale, y_scale, _: x_scale
+        self.parameters["w_0"].scale_func = lambda x_scale, y_scale, _: x_scale
 
     def func(
         self, x: Array[("num_samples",), np.float64], param_values: Dict[str, float]
     ) -> Array[("num_samples",), np.float64]:
+        """
+        Return the probability of observation of readout event.
+
+        :param x: Angular frequency of driving field
+        """
         t_pulse = param_values["t_pulse"]
         del param_values["t_pulse"]
         return super()._func((t_pulse, x), **param_values)
@@ -212,7 +199,6 @@ class RabiFlopFreq(RabiFlop):
         :param model_parameters: dictionary mapping model parameter names to their
             metadata.
         """
-        # TODO: Automatically estimate readout levels
         model_parameters["P_readout_e"].initialise(1.0)
         model_parameters["P_readout_g"].initialise(0.0)
         model_parameters["t_dead"].initialise(0.0)
@@ -231,29 +217,32 @@ class RabiFlopFreq(RabiFlop):
         a = np.abs(fit.values["a"])
         t_pulse = model_parameters["t_pulse"].initialise(2 * fit.values["w"])
         model_parameters["omega"].initialise(2 * np.sqrt(a) / t_pulse)
-        model_parameters["freq_offset"].initialise(-fit.values["x0"])
+        model_parameters["w_0"].initialise(-fit.values["x0"])
 
 
 class RabiFlopTime(RabiFlop):
     def __init__(self, start_excited=True):
         super().__init__(start_excited)
 
-        self.parameters["freq"] = ModelParameter()
+        self.parameters["w"] = ModelParameter()
 
-        self.parameters["freq"].scale_func = lambda x_scale, y_scale, _: 1 / x_scale
+        self.parameters["w"].scale_func = lambda x_scale, y_scale, _: 1 / x_scale
         self.parameters["omega"].scale_func = lambda x_scale, y_scale, _: 1 / x_scale
         self.parameters["tau"].scale_func = lambda x_scale, y_scale, _: 1 / x_scale
         self.parameters["t_dead"].scale_func = lambda x_scale, y_scale, _: x_scale
-        self.parameters["freq_offset"].scale_func = (
-            lambda x_scale, y_scale, _: 1 / x_scale
-        )
+        self.parameters["w_0"].scale_func = lambda x_scale, y_scale, _: 1 / x_scale
 
     def func(
         self, x: Array[("num_samples",), np.float64], param_values: Dict[str, float]
     ) -> Array[("num_samples",), np.float64]:
-        freq = param_values["freq"]
-        del param_values["freq"]
-        return super()._func((x, freq), **param_values)
+        """
+        Return the probability of observation of readout event.
+
+        :param x: Pulse duration of driving field
+        """
+        w = param_values["w"]
+        del param_values["w"]
+        return super()._func((x, w), **param_values)
         # pytype: disable=wrong-arg-types
 
     def estimate_parameters(
@@ -279,7 +268,6 @@ class RabiFlopTime(RabiFlop):
         :param model_parameters: dictionary mapping model parameter names to their
             metadata.
         """
-        # TODO: Automatically estimate readout levels
         model_parameters["P_readout_e"].initialise(1.0)
         model_parameters["P_readout_g"].initialise(0.0)
         model_parameters["t_dead"].initialise(0.0)
@@ -297,5 +285,5 @@ class RabiFlopTime(RabiFlop):
         delta = 0.0 if omega >= W else np.sqrt(W**2 - omega**2)
 
         model_parameters["omega"].initialise(omega)
-        model_parameters["freq_offset"].initialise(0.0)
-        model_parameters["freq"].initialise(delta)
+        model_parameters["w_0"].initialise(0.0)
+        model_parameters["w"].initialise(delta)
