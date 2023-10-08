22a5a974613d04ec53b12624ffb213c364119bfd
Thomas Harty <thomas.peter.harty@gmail.com>
2023-10-08 23:21:00 +0100

    test_binomial: add a test based on random sampling

--- test/test_binomial.py
+++ test/test_binomial.py
@@ -1,11 +1,13 @@
 import numpy as np
+from matplotlib import pyplot as plt
+from scipy import stats
 
 import ionics_fits as fits
 from . import common
 
 
 def test_binomial(plot_failures):
-    """Test binomial fitting"""
+    """Basic test of binomial fitting"""
     num_trials = 1000
     x = np.linspace(-3, 3, 200) * 2 * np.pi
     model = fits.models.Sinusoid()
@@ -28,3 +30,51 @@ def test_binomial(plot_failures):
         fitter_cls=fits.BinomialFitter,
         fitter_args={"num_trials": num_trials},
     )
+
+
+def test_binomial_synthetic(plot_failures):
+    """Test binomial fitting to multiple synthetic datasets"""
+    num_trials = 100
+    x = np.linspace(-3, 3, 200) * 2 * np.pi
+    model = fits.models.Sinusoid()
+    params = {
+        "a": 0.5,
+        "omega": 1,
+        "phi": 1,
+        "y0": 0.5,
+        "x0": 0,
+        "tau": np.inf,
+    }
+    model.parameters["a"].fixed_to = params["a"]
+    model.parameters["y0"].fixed_to = params["y0"]
+
+    model.parameters["omega"].lower_bound = 0
+    model.parameters["omega"].upper_bound = 10
+    model.parameters["phi"].lower_bound = 0
+    model.parameters["phi"].upper_bound = 2
+    model.parameters["x0"].lower_bound = 0
+    model.parameters["x0"].upper_bound = 1
+
+    y_model = model.func(x, params)
+
+    num_samples = 1000
+    phi_fit = np.zeros(num_samples)
+
+    for sample in range(num_samples):
+        y = stats.binom.rvs(n=num_trials, p=y_model, size=y_model.size)
+        y = y / num_trials
+
+        fit = fits.BinomialFitter(x=x, y=y, num_trials=num_trials, model=model)
+
+        phi_fit[sample] = fit.values["phi"]
+
+    plt.hist(phi_fit, bins=50)
+    plt.axvline(x=1, color="black")
+    plt.axvline(x=1 + fit.uncertainties["phi"], color="black", linestyle="--")
+    plt.axvline(x=1 - fit.uncertainties["phi"], color="black", linestyle="--")
+    plt.grid()
+    plt.show()
+
+
+if __name__ == "__main__":
+    test_binomial_synthetic(True)
