f79a2098c5aa4da3e8598474e71810fa652e746c
Thomas Harty <thomas.peter.harty@gmail.com>
2024-01-27 22:43:20 +0000

    test_scaled_model: tweak

--- test/test_scaled_model.py
+++ test/test_scaled_model.py
@@ -10,13 +10,13 @@ def test_scaled_model(plot_failures):
     params = {"a": 2.5, "omega": 3.3, "phi": 1, "y0": -6, "tau": np.inf, "x0": 0}
 
     model = fits.models.Sinusoid()
-    scaled_model = fits.models.ScaledModel(model=model, x_scale=2)
+    scaled_model = fits.models.ScaledModel(model=model, x_scale=4)
 
-    common.is_close(model(2 * x, **params), scaled_model(x, **params), 1e-9)
+    assert common.is_close(model(4 * x, **params), scaled_model(x, **params), 1e-9)
 
     common.check_single_param_set(
         x=x,
         model=scaled_model,
         test_params=params,
-        config=common.TestConfig(plot_failures=plot_failures, heuristic_tol=0.1),
+        config=common.TestConfig(plot_failures=plot_failures, heuristic_tol=0.2),
     )
